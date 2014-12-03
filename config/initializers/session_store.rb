d0576d69c4ca6be8d6dfb0f57a63a83ebfdd3322
philipslan <philiplan2017@u.northwestern.edu>
2014-12-03 03:25:53 -0600

    changed name of application

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_starterleague_session'
+Rails.application.config.session_store :cookie_store, key: '_wett_session'
